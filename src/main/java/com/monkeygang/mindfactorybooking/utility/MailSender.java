7318f88bc198ba3aec59950b97dc222095f9277f
LukasH0lm <lukasholm@hotmail.com>
2023-05-25 19:02:16 +0200

    removed comments

--- src/main/java/com/monkeygang/mindfactorybooking/utility/MailSender.java
+++ src/main/java/com/monkeygang/mindfactorybooking/utility/MailSender.java
@@ -22,18 +22,14 @@ public class MailSender {
 
     public static void sendReceiptToCustomer() throws IOException {
 
-        // Recipient's email
         String to = CurrentBookingSingleton.getInstance().getCustomer().getEmail();
 
-        // Sender's email
         String from = "MindFactory@ecco.dk";
 
         String host = "smtp-relay.sendinblue.com";
 
-        // Get system properties
         Properties properties = System.getProperties();
 
-        // Setup mail server
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.ssl.enable", "no");
@@ -48,7 +44,6 @@ public class MailSender {
         System.out.println(password);
 
 
-        // Get the Session object.// and pass username and password
         Session session = Session.getInstance(properties, new Authenticator() {
 
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -59,36 +54,27 @@ public class MailSender {
 
         });
 
-        // Used to debug SMTP issues
         session.setDebug(true);
 
         try {
-            // Create a default MimeMessage object.
             MimeMessage message = new MimeMessage(session);
 
-            // Set From: header field of the header.
             message.setFrom(new InternetAddress(from));
 
-            // Set To: header field of the header.
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
-            // Set Subject: header field
             message.setSubject("MindFactory Booking kode");
 
             // Now set the actual message
             message.setText("Din kode er: " + CurrentBookingSingleton.getInstance().getBooking().hashCode());
 
 
-            // Create the message part
             BodyPart messageBodyPart = new MimeBodyPart();
 
-            // Now set the actual message
             messageBodyPart.setText("hvis du vil ændre i din booking, skal du bruge denne kode: ");
 
-// Create a multipar message
             Multipart multipart = new MimeMultipart();
 
-            // Set text message part
             multipart.addBodyPart(messageBodyPart);
 
             /*
@@ -100,12 +86,11 @@ public class MailSender {
             messageBodyPart.setFileName(filename);
             multipart.addBodyPart(messageBodyPart);*/
 
-            // Send the complete message parts
             message.setContent(multipart);
 
 
             System.out.println("sending...");
-            // Send message
+            // Sender beskeden
             Transport.send(message);
             System.out.println("Sent message successfully....");
         } catch (MessagingException mex) {
